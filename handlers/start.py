b75e42d9763515121bae726098432cb9746c53a6
qqesc1337 <lyakun.i@yandex.ru>
2026-06-17 20:17:37 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from database import SessionLocal, User
