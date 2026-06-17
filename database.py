26bdd1973fea817d10af8814af6c38d9ffa9265e
qqesc1337 <lyakun.i@yandex.ru>
2026-06-17 18:22:36 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
-from config import DATABASE_URL
 import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///tsum_auction.db")
 
 if "postgres" in DATABASE_URL:
     engine = create_engine(DATABASE_URL)
