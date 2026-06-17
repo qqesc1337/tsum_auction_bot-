5a5ed7f780ba6a182cfd58337a302c3acf690fbf
qqesc1337 <lyakun.i@yandex.ru>
2026-06-17 22:27:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config import BOT_TOKEN
+from handlers.main_handlers import router
+from scheduler import start_scheduler
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
-
-@dp.message(F.text == "/start")
-async def start(message: Message):
-    await message.answer("✅ БОТ РАБОТАЕТ! Напиши что-нибудь ещё.")
-
-@dp.message()
-async def echo(message: Message):
-    await message.answer(f"Ты написал: {message.text}")
+dp.include_router(router)
 
 async def main():
-    logger.info("🚀 ТЕСТОВЫЙ БОТ ЗАПУЩЕН!")
+    start_scheduler()
+    logger.info("🚀 Бот TSUM Auction запущен!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
